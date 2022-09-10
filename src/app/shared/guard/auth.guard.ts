224e6255009701e40fe5e06ccbf80253373b85b3
Johnny Silva <johnnysilva.dev@gmail.com>
2022-09-10 17:44:35 -0300

    ajuste canActivate

--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -16,6 +16,6 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    return this.authService.isLoggedIn;
   }
 }
